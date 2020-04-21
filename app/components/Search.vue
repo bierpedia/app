526aa8a7a65ecf9dc374c87e552c03871b7555cf
Matthias <matthias@kasperidus.de>
2020-04-22 01:13:11 +0200

    Vue class component for Search.vue

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -1,50 +1,58 @@
 <template android>
   <Page actionBarHidden="true">
-      <StackLayout backgroundColor="#3c495e">
-    <SearchBar hint="Search hint" v-model="searchQuery" @textChange="onTextChanged" @submit="onSubmit" />
-    <ActivityIndicator busy="true" @busyChange="onBusyChanged" />
-    <Label>Suchergebnisse für die Anfrage: {{searchQuery}} </Label>
-    <ListView ref="" for="item in searchResults" @itemTap="onItemTap">
-      <v-template>
-        <Label :text="item.text" />
-      </v-template>
-    </ListView>
-      </StackLayout>
+    <StackLayout backgroundColor="#3c495e">
+      <SearchBar
+        hint="Search hint"
+        v-model="searchQuery"
+        @textChange="onTextChanged"
+        @submit="onSubmit"
+      />
+      <ActivityIndicator busy="true" @busyChange="onBusyChanged" />
+      <Label>Suchergebnisse für die Anfrage: {{searchQuery}}</Label>
+      <ListView ref for="item in searchResults" @itemTap="onItemTap">
+        <v-template>
+          <Label :text="item.text" />
+        </v-template>
+      </ListView>
+    </StackLayout>
   </Page>
 </template>
 
 <script lang="ts">
-import Beer from './Modals/Beer'
-export default {
-  components:{
-    Beer
-  },
-  data() {
-    return {
-      searchQuery: "",
-      searchResults: []
-    };
-  },
-  methods: {
-    onSubmit() {
-      console.log("onSubmit");
-      this.searchResults = [{
-          text: 'Entry 1'},{
-          text: 'Entry 2'},{
-          text: 'Entry 3'},{
-          text: 'Entry 4'},{
-          text: 'Entry 5'}
-        ]
-    },
-    onTextChanged() {
-      console.log("onTextChanged");
-      
-    },
-    onItemTap() {
-        this.$showModal(Beer);
-    }
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+import Beer from "./Modals/Beer";
+
+@Component({
+  components: { Beer }
+})
+export default class Search extends Vue {
+  @Prop() private searchQuery: string = "";
+  @Prop() private searchResults = [];
+
+  onSubmit() {
+    console.log("onSubmit");
+    this.searchResults = [
+      {
+        text: "Entry 1"
+      },
+      {
+        text: "Entry 2"
+      },
+      {
+        text: "Entry 3"
+      }
+    ];
   }
-};
+
+  onTextChanged() {
+    console.log("onTextChanged");
+  }
+
+  onItemTap() {
+    this.$showModal(Beer);
+  }
+}
 </script>
 
 <style scoped>
